0862a055302e6facdb57cc4d797edc78b43c516e
Sheila Chepkemoi <Sheila.chepkemoi@smart.local>
2024-03-21 11:33:52 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './layouts/topbar/topbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomepageComponent } from './feature/pages/homepage/homepage.component';
+import { TaskmanagerComponent } from './feature/pages/taskmanager/taskmanager.component';
+import { LoginComponent } from './account/auth/login/login.component';
+import { RegistrationComponent } from './account/auth/registration/registration.component';
+import { ForgotpasswordComponent } from './account/auth/forgotpassword/forgotpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomepageComponent,
+    TaskmanagerComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ForgotpasswordComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feature/pages/taskmanager/taskmanager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taskmanager',
+  templateUrl: './taskmanager.component.html',
+  styleUrls: ['./taskmanager.component.scss']
+})
+export class TaskmanagerComponent {
+
+}
